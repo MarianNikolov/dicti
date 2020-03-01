0a91ab8f0563614619972e493d9418c1c65e3839
Martina_Shebova <martina2314@gmail.com>
2020-03-01 19:01:56 +0200

    Added prod general api core url

--- Dicti.Web/ClientApp/src/index.js
+++ Dicti.Web/ClientApp/src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import RestService from './RestServices/restService';
 import dev_config from './config/development.config.json';
+import prod_config from './config/production.config.json';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
@@ -13,6 +14,10 @@ const rootElement = document.getElementById('root');
 console.log(process.env)
 if (process.env.NODE_ENV === "development") {
 	RestService.setDomainUrl(dev_config)
+}
+
+if (process.env.NODE_ENV === "production") {
+	RestService.setDomainUrl(prod_config)
 }
 
 ReactDOM.render(
